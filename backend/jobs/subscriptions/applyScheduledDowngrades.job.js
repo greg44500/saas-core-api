764df2c64ec30387eedcf25f1f23f5a7fa09ce9f
greg44500 <gregdevweb44500@gmail.com>
2026-09-08 14:45:15 +0200

    docs(backend): document scheduled downgrade job

--- backend/jobs/subscriptions/applyScheduledDowngrades.job.js
+++ backend/jobs/subscriptions/applyScheduledDowngrades.job.js
@@ -2,6 +2,26 @@ import {
     applyScheduledDowngrades,
 } from '../../modules/subscriptions/services/applyScheduledDowngrades.service.js';
 
+/**
+ * Exécute le runner opérationnel chargé d'appliquer les downgrades planifiés.
+ *
+ * Le calcul d'éligibilité, l'application du plan cible et les garanties de
+ * cohérence appartiennent au service `applyScheduledDowngrades`. Ce wrapper
+ * fournit seulement les paramètres d'exécution, journalise le résultat et
+ * propage toute erreur afin qu'un ordonnanceur externe puisse constater
+ * l'échec du job.
+ *
+ * `now` rend l'instant de bascule déterministe pour les tests et les reprises
+ * contrôlées. `batchSize` limite le volume traité sans modifier la règle
+ * commerciale. La rejouabilité, l'idempotence et la concurrence restent sous
+ * l'autorité du service métier appelé.
+ *
+ * @param {object} [options]
+ * @param {Date} [options.now] Instant de référence pour les échéances.
+ * @param {number} [options.batchSize] Taille de lot éventuellement imposée.
+ * @param {{info: Function, error: Function}} [options.logger] Logger opérationnel.
+ * @returns {Promise<object>} Résultat retourné par le service de downgrade.
+ */
 const runApplyScheduledDowngradesJob = async ({
     now = new Date(),
     batchSize,
