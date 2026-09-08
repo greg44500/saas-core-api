6229e4543fd5df410fd6ec154c20a06e3e467ef1
greg44500 <gregdevweb44500@gmail.com>
2026-09-08 17:29:22 +0200

    security(d020): keep invitation token out of query string

--- backend/modules/commercialInvitation/commercialInvitationUrl.js
+++ backend/modules/commercialInvitation/commercialInvitationUrl.js
@@ -4,6 +4,11 @@ import { env } from '../../config/env.js';
  * Le lien de sécurité est toujours construit depuis CLIENT_URL. L'hôte HTTP
  * reçu dans la requête n'est jamais réutilisé afin d'éviter l'empoisonnement
  * d'URL dans les emails transactionnels.
+ *
+ * Le secret est placé dans le fragment plutôt que dans la query string : le
+ * navigateur ne transmet pas ce fragment au serveur HTTP. Le frontend doit le
+ * lire, le conserver uniquement en mémoire le temps du parcours puis nettoyer
+ * l'URL avec history.replaceState.
  */
 const buildCommercialInvitationUrl = ({ token }) => {
     const invitationUrl = new URL(
@@ -11,7 +16,7 @@ const buildCommercialInvitationUrl = ({ token }) => {
         env.CLIENT_URL,
     );
 
-    invitationUrl.searchParams.set('token', token);
+    invitationUrl.hash = new URLSearchParams({ token }).toString();
 
     return invitationUrl.toString();
 };
