b637fa210560203be4926e531d38d45801ea2259
greg44500 <gregdevweb44500@gmail.com>
2026-09-10 19:36:50 +0200

    feat(legal): define canonical registration document versions

---
+++ backend/constants/legalDocuments.constants.js
@@ -0,0 +1,14 @@
+const LEGAL_DOCUMENT_VERSION = Object.freeze({
+    TERMS: '2026-09-10',
+    PRIVACY_POLICY: '2026-09-10',
+});
+
+const LEGAL_ACCEPTANCE_SOURCE = Object.freeze({
+    LOCAL_REGISTRATION: 'local_registration',
+    COMMERCIAL_INVITATION_REGISTRATION: 'commercial_invitation_registration',
+});
+
+export {
+    LEGAL_ACCEPTANCE_SOURCE,
+    LEGAL_DOCUMENT_VERSION,
+};
