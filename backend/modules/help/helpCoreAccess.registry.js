53f89c0047e220a5804a18554f80cd2004d28ae4
greg44500 <gregdevweb44500@gmail.com>
2026-09-16 13:53:45 +0200

    fix(help): declare remediation-safe core help entries

---
+++ backend/modules/help/helpCoreAccess.registry.js
@@ -0,0 +1,17 @@
+const CORE_WORKSPACE_REMEDIATION_HELP_ENTRY_IDS = Object.freeze([
+    'workspace.account.preferences',
+    'workspace.account.security',
+    'workspace.members.remove',
+    'workspace.ownership.transfer',
+    'workspace.archive',
+    'workspace.files.restore',
+    'workspace.files.delete_permanently',
+    'workspace.activity.read',
+    'workspace.subscription.read',
+    'workspace.subscription.trial',
+    'workspace.subscription.cancel',
+    'workspace.subscription.downgrade',
+]);
+
+
+export { CORE_WORKSPACE_REMEDIATION_HELP_ENTRY_IDS };
